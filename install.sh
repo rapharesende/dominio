7f818ae25caf46a8d5d9a2cfbdf54c565097ccde
rapharesende <rapharesende@gmail.com>
2019-06-24 11:02:54 -0300

    first commit

---
+++ install.sh
@@ -0,0 +1,1 @@
+yum -y install sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools krb5-workstation openldap-clients policycoreutils-python samba-common-tools  policycoreutils-devel 
