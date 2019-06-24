32bcc7074647b085d5a2be461a5908b3fa201fda
rapharesende <36664766+rapharesende@users.noreply.github.com>
2019-06-24 11:09:00 -0300

    Delete install.sh

--- install.sh
+++
@@ -1,2 +0,0 @@
-yum -y install sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools krb5-workstation openldap-clients\
-policycoreutils-python samba-common-tools  policycoreutils-devel 
