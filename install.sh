1123f1910f87009105af4a3e881b80a5ad43e5e5
rapharesende <36664766+rapharesende@users.noreply.github.com>
2019-06-24 11:03:45 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
-yum -y install sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools krb5-workstation openldap-clients policycoreutils-python samba-common-tools  policycoreutils-devel 
+yum -y install sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools krb5-workstation openldap-clients\
+policycoreutils-python samba-common-tools  policycoreutils-devel 
